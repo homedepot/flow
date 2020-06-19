925913cfc7c7dafbee99beeb3ed2c3cfc93f0cc2
Laura Jackson <pair+ljackson@homedepot.com>
2020-06-19 13:46:16 -0400

    Adding additional log for support multiple projects from tracker.

--- tests/projecttracking/tracker/test_tracker.py
+++ tests/projecttracking/tracker/test_tracker.py
@@ -2,9 +2,10 @@ import configparser
 import json
 import os
 from unittest.mock import MagicMock
+from unittest.mock import Mock
 from unittest.mock import patch
-
 import pytest
+
 from flow.projecttracking.tracker.tracker import Tracker
 
 from flow.buildconfig import BuildConfig
@@ -16,6 +17,27 @@ mock_build_config_dict = {
     "projectTracking": {
         "tracker": {
             "projectId": 123456
+        }
+    },
+    "environments": {
+        "unittest": {
+            "artifactCategory": "release"
+        }
+    },
+    "slack": {
+        "botName": "Flow",
+        "emoji": ":robot_face:",
+        "channel": "#spigot-ci"
+    }
+}
+
+mock_build_config_dict_multi_projects = {
+    "projectInfo": {
+        "name": "testproject"
+    },
+    "projectTracking": {
+        "tracker": {
+            "projectIds": [123456, 423476]
         }
     },
     "environments": {
@@ -46,12 +68,34 @@ mock_build_config_missing_tracker_dict = {
     }
 }
 
-mock_build_config_missing_projectid_dict = {
+mock_build_config_missing_project_id_dict = {
     "projectInfo": {
         "name": "testproject"
     },
     "projectTracking": {
         "tracker": {
+        }
+    },
+    "environments": {
+        "unittest": {
+            "artifactCategory": "release"
+        }
+    },
+    "slack": {
+        "botName": "Flow",
+        "emoji": ":robot_face:",
+        "channel": "#spigot-ci"
+    }
+}
+
+mock_build_config_dict_both_project_ids = {
+    "projectInfo": {
+        "name": "testproject"
+    },
+    "projectTracking": {
+        "tracker": {
+            "projectId": 123456,
+            "projectIds": [654321, 999999]
         }
     },
     "environments": {
@@ -72,21 +116,178 @@ url = https://www.pivotaltracker.com
 """
 
 
+def mock_get_multiple_project_story_details_response(*args, **kwargs):
+    current_test_directory = os.path.dirname(os.path.realpath(__file__))
+    with open(current_test_directory + "/tracker_stories_bug.json", 'r') as myfile:
+        tracker_data = myfile.read()
+
+    _response_mock = Mock()
+    _response_mock.text = tracker_data
+    if args[0] == 'http://happy.happy.joy.joy/services/v5/projects/123456/stories/134082057':
+        _response_mock.status_code = 200
+    elif args[0] == 'http://happy.happy.joy.joy/services/v5/projects/123456/stories/134082058':
+        _response_mock.status_code = 200
+    elif args[0] == 'http://happy.happy.joy.joy/services/v5/projects/123456/stories/264082058':
+        _response_mock.text = ''
+        _response_mock.status_code = 404
+    elif args[0] == 'http://happy.happy.joy.joy/services/v5/projects/423476/stories/264082058':
+        _response_mock.status_code = 200
+    else:
+        _response_mock.text = []
+        _response_mock.status_code = 500
+
+    return _response_mock
+
+
+def mock_get_multiple_project_labels_response(*args, **kwargs):
+    _response_mock = Mock()
+    _response_mock.text = ''
+    if args[0] == 'http://happy.happy.joy.joy/services/v5/projects/123456/stories/134082057':
+        _response_mock.status_code = 200
+    elif args[0] == 'http://happy.happy.joy.joy/services/v5/projects/423476/stories/134082057':
+        _response_mock.status_code = 404
+    elif args[0] == 'http://happy.happy.joy.joy/services/v5/projects/123456/stories/222082037':
+        _response_mock.status_code = 404
+    elif args[0] == 'http://happy.happy.joy.joy/services/v5/projects/423476/stories/222082037':
+        _response_mock.status_code = 200
+    else:
+        _response_mock.status_code = 500
+
+    return _response_mock
+
+
 def test_no_initialize_object(monkeypatch):
     monkeypatch.setenv('TRACKER_TOKEN', 'fake_token')
 
     _b = MagicMock(BuildConfig)
     _b.build_env_info = mock_build_config_dict['environments']['unittest']
     _b.json_config = mock_build_config_dict
-
     _tracker = Tracker(config_override=_b)
     assert _tracker is not None
 
 
-# def test_get_story_details_from_commits():
-#     _tracker = Tracker()
-#     story_details = _tracker.get_story_details_from_commits(commit_history=["134082057", "134082058"])
-#     assert len(story_details) == 2
+def test_get_details_for_all_stories(monkeypatch):
+    monkeypatch.setenv('TRACKER_TOKEN', 'fake_token')
+
+    _b = MagicMock(BuildConfig)
+    _b.build_env_info = mock_build_config_dict['environments']['unittest']
+    _b.json_config = mock_build_config_dict
+
+    parser = configparser.ConfigParser()
+    parser.add_section('tracker')
+    parser.set('tracker', 'url', 'http://happy.happy.joy.joy')
+    _b.settings = parser
+    with patch('requests.get') as mock_request:
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'X-TrackerToken': 'fake_token'}
+        timeout = 30
+        current_test_directory = os.path.dirname(os.path.realpath(__file__))
+        with open(current_test_directory + "/tracker_stories_bug.json", 'r') as myfile:
+            tracker_data = myfile.read()
+        mock_request.return_value.text = tracker_data
+        mock_request.return_value.status_code = 200
+        _tracker = Tracker(config_override=_b)
+        story_details = _tracker.get_details_for_all_stories(story_list=["134082057", "134082058"])
+        mock_request.assert_any_call('http://happy.happy.joy.joy/services/v5/projects/123456/stories/134082057',
+                                     headers=headers, timeout=timeout)
+        mock_request.assert_any_call('http://happy.happy.joy.joy/services/v5/projects/123456/stories/134082058',
+                                     headers=headers, timeout=timeout)
+        assert story_details[0] == json.loads(tracker_data)
+
+
+def test_get_details_for_all_stories_for_multiple_projects(monkeypatch):
+    monkeypatch.setenv('TRACKER_TOKEN', 'fake_token')
+
+    _b = MagicMock(BuildConfig)
+    _b.build_env_info = mock_build_config_dict_multi_projects['environments']['unittest']
+    _b.json_config = mock_build_config_dict_multi_projects
+
+    parser = configparser.ConfigParser()
+    parser.add_section('tracker')
+    parser.set('tracker', 'url', 'http://happy.happy.joy.joy')
+    _b.settings = parser
+    with patch('requests.get') as mock_request:
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'X-TrackerToken': 'fake_token'}
+        timeout = 30
+        current_test_directory = os.path.dirname(os.path.realpath(__file__))
+        with open(current_test_directory + "/tracker_stories_bug.json", 'r') as myfile:
+            tracker_data = myfile.read()
+        _tracker = Tracker(config_override=_b)
+        mock_request.side_effect = mock_get_multiple_project_story_details_response
+
+        story_details = _tracker.get_details_for_all_stories(story_list=["134082057", "134082058", "264082058"])
+        # assert mock_request.call_counts == 4
+        mock_request.assert_any_call('http://happy.happy.joy.joy/services/v5/projects/123456/stories/134082057',
+                                     headers=headers, timeout=timeout)
+        mock_request.assert_any_call('http://happy.happy.joy.joy/services/v5/projects/123456/stories/134082058',
+                                     headers=headers, timeout=timeout)
+        mock_request.assert_any_call('http://happy.happy.joy.joy/services/v5/projects/123456/stories/264082058',
+                                     headers=headers, timeout=timeout)
+        mock_request.assert_any_call('http://happy.happy.joy.joy/services/v5/projects/423476/stories/264082058',
+                                     headers=headers, timeout=timeout)
+        assert story_details[0] == json.loads(tracker_data)
+
+
+def test_tag_stories_in_commit(monkeypatch):
+    monkeypatch.setenv('TRACKER_TOKEN', 'fake_token')
+
+    _b = MagicMock(BuildConfig)
+    _b.build_env_info = mock_build_config_dict['environments']['unittest']
+    _b.project_name = mock_build_config_dict['projectInfo']['name']
+    _b.version_number = 'v1.0'
+    _b.json_config = mock_build_config_dict
+
+    parser = configparser.ConfigParser()
+    parser.add_section('tracker')
+    parser.set('tracker', 'url', 'http://happy.happy.joy.joy')
+    _b.settings = parser
+    with patch('requests.post') as mock_request:
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'X-TrackerToken': 'fake_token'}
+        timeout = 30
+        label = {
+                    "name": "testproject-v1.0"
+                }
+        json_label = json.dumps(label, default=lambda o: o.__dict__, sort_keys=False, indent=4)
+        mock_request.return_value.text = ''
+        mock_request.return_value.status_code = 200
+
+        _tracker = Tracker(config_override=_b)
+        _tracker.tag_stories_in_commit(story_list=['134082057', '134082058'])
+        mock_request.assert_any_call('http://happy.happy.joy.joy/services/v5/projects/123456/stories/134082057/labels',
+                                     json_label, headers=headers, timeout=timeout)
+        mock_request.assert_any_call('http://happy.happy.joy.joy/services/v5/projects/123456/stories/134082058/labels',
+                                     json_label, headers=headers, timeout=timeout)
+
+
+def test_tag_stories_in_commit_for_multiple_projects(monkeypatch):
+    monkeypatch.setenv('TRACKER_TOKEN', 'fake_token')
+
+    _b = MagicMock(BuildConfig)
+    _b.build_env_info = mock_build_config_dict_multi_projects['environments']['unittest']
+    _b.project_name = mock_build_config_dict_multi_projects['projectInfo']['name']
+    _b.version_number = 'v1.0'
+    _b.json_config = mock_build_config_dict_multi_projects
+
+    parser = configparser.ConfigParser()
+    parser.add_section('tracker')
+    parser.set('tracker', 'url', 'http://happy.happy.joy.joy')
+    _b.settings = parser
+    with patch('requests.post') as mock_request:
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'X-TrackerToken': 'fake_token'}
+        timeout = 30
+        label = {
+                    "name": "testproject-v1.0"
+                }
+        json_label = json.dumps(label, default=lambda o: o.__dict__, sort_keys=False, indent=4)
+        mock_request.return_value.text = ''
+        mock_request.return_value.status_code = 200
+        mock_request.side_effect = mock_get_multiple_project_labels_response
+
+        _tracker = Tracker(config_override=_b)
+        _tracker.tag_stories_in_commit(story_list=['134082057', '222082037'])
+        mock_request.assert_any_call('http://happy.happy.joy.joy/services/v5/projects/123456/stories/134082057/labels',
+                                     json_label, headers=headers, timeout=timeout)
+        mock_request.assert_any_call('http://happy.happy.joy.joy/services/v5/projects/423476/stories/222082037/labels',
+                                     json_label, headers=headers, timeout=timeout)
 
 
 def test_story_bump_bug(monkeypatch):
@@ -140,6 +341,22 @@ def test_story_bump_major(monkeypatch):
     assert bump_type == "major"
 
 
+def test_init_for_multiple_projects_too_many_project_id_keys(monkeypatch):
+    monkeypatch.setenv('TRACKER_TOKEN', 'fake_token')
+
+    _b = MagicMock(BuildConfig)
+    _b.build_env_info = mock_build_config_dict_both_project_ids['environments']['unittest']
+    _b.json_config = mock_build_config_dict_both_project_ids
+
+    with patch('flow.utils.commons.print_msg') as mock_printmsg_fn:
+        with pytest.raises(SystemExit):
+            Tracker(config_override=_b)
+
+        mock_printmsg_fn.assert_called_with('Tracker', '__init__',
+                                            "The build config may only contain 'projectId' for single project id"
+                                            "or 'projectIds' containing an array of project ids", 'ERROR')
+
+
 def test_init_missing_tracker(monkeypatch):
     monkeypatch.setenv('TRACKER_TOKEN', 'fake_token')
 
@@ -148,25 +365,27 @@ def test_init_missing_tracker(monkeypatch):
 
     with patch('flow.utils.commons.print_msg') as mock_printmsg_fn:
         with pytest.raises(SystemExit):
-
             Tracker(config_override=_b)
 
-        mock_printmsg_fn.assert_called_with('Tracker', '__init__', "The build config associated with projectTracking is "
-                                                                   "missing key 'projectTracking'", 'ERROR')
+        mock_printmsg_fn.assert_called_with('Tracker', '__init__',
+                                            "The build config associated with projectTracking is "
+                                            "missing key 'projectTracking'", 'ERROR')
 
 
 def test_init_missing_tracker_project_id(monkeypatch):
     monkeypatch.setenv('TRACKER_TOKEN', 'fake_token')
 
     _b = MagicMock(BuildConfig)
-    _b.json_config = mock_build_config_missing_projectid_dict
+    _b.json_config = mock_build_config_missing_project_id_dict
 
     with patch('flow.utils.commons.print_msg') as mock_printmsg_fn:
         with pytest.raises(SystemExit):
-
             Tracker(config_override=_b)
 
-        mock_printmsg_fn.assert_called_with('Tracker', '__init__', "The build config associated with projectTracking is missing key 'projectId'", 'ERROR')
+        mock_printmsg_fn.assert_called_with('Tracker', '__init__',
+                                            "The build config associated with projectTracking is missing key "
+                                            "'projectId'",
+                                            'ERROR')
 
 
 def test_init_missing_tracker_url(monkeypatch):
@@ -179,10 +398,10 @@ def test_init_missing_tracker_url(monkeypatch):
 
     with patch('flow.utils.commons.print_msg') as mock_printmsg_fn:
         with pytest.raises(SystemExit):
-
             Tracker(config_override=_b)
 
-        mock_printmsg_fn.assert_called_with('Tracker', '__init__', 'No tracker url found in buildConfig or settings.ini.', 'ERROR')
+        mock_printmsg_fn.assert_called_with('Tracker', '__init__',
+                                            'No tracker url found in buildConfig or settings.ini.', 'ERROR')
 
 
 def test_init_missing_tracker_url_but_contains_tracker_in_config_parser(monkeypatch):
@@ -196,10 +415,10 @@ def test_init_missing_tracker_url_but_contains_tracker_in_config_parser(monkeypa
 
     with patch('flow.utils.commons.print_msg') as mock_printmsg_fn:
         with pytest.raises(SystemExit):
-
             Tracker(config_override=_b)
 
-        mock_printmsg_fn.assert_called_with('Tracker', '__init__', 'No tracker url found in buildConfig or settings.ini.', 'ERROR')
+        mock_printmsg_fn.assert_called_with('Tracker', '__init__',
+                                            'No tracker url found in buildConfig or settings.ini.', 'ERROR')
 
 
 def test_init_missing_env_variable(monkeypatch):
@@ -208,7 +427,8 @@ def test_init_missing_env_variable(monkeypatch):
 
     with patch('flow.utils.commons.print_msg') as mock_printmsg_fn:
         with pytest.raises(SystemExit):
-
             Tracker()
 
-        mock_printmsg_fn.assert_called_with('Tracker', '__init__', "No tracker token found in environment.  Did you define environment variable 'TRACKER_TOKEN'?", 'ERROR')
+        mock_printmsg_fn.assert_called_with('Tracker', '__init__',
+                                            "No tracker token found in environment.  Did you define environment variable 'TRACKER_TOKEN'?",
+                                            'ERROR')
